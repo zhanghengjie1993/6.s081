e14d08bca906e61862175449c73874cdf2448cdf
hengjie <1920515424@qq.com>
2021-01-10 20:03:13 +0800

    fix: 更新

--- kernel/vm.c
+++ kernel/vm.c
@@ -440,3 +440,29 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void _vmprint(pagetable_t pagetable, int level)
+{
+  for (int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    if ((pte & PTE_V) != 0){
+      pagetable_t child = (pagetable_t)PTE2PA(pte);
+      if (level == 0){
+        printf("..%d: pte %p pa %p\n", i, pte, child);
+        _vmprint(child, level+1);
+      }
+      else if (level == 1){
+        printf(".. ..%d: pte %p pa %p\n", i, pte, child);
+        _vmprint(child, level+1);
+      }
+      else
+        printf(".. .. ..%d: pte %p pa %p\n", i, pte, child);    
+    }
+  }
+}
+
+void vmprint(pagetable_t pagetable)
+{
+  printf("page table %p\n", pagetable);
+  _vmprint(pagetable, 0);
+}
